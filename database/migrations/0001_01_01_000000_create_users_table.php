f203ff838614271f46ccedf7f47e55727e509db6
Ica <2206041@itg.ac.id>
2025-09-20 11:42:10 +0700

    feat: Create database migrations for stores, products, and links

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            // Baris ini ditambahkan untuk menyimpan peran pengguna
+            $table->enum('role', ['admin', 'penjual', 'pengguna'])->default('pengguna');
             $table->rememberToken();
             $table->timestamps();
         });
